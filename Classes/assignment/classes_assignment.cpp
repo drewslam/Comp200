a8402a27ced647a1cb29713579c86bea47c93f26
Drewslam <souzaphonix@pm.me>
2023-10-15 13:56:19 -0700

    Submitted final assignment draft

--- Classes/assignment/classes_assignment.cpp
+++ Classes/assignment/classes_assignment.cpp
@@ -1,6 +1,6 @@
-///
-///
-///
+/// Andrew Souza
+/// Comp 200 -- Spring 23
+/// Classes Assignment
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -27,18 +27,22 @@ class CashRegister {
         vector<double> items;
 };
 
+// Default constructor that produces an empty vector
 CashRegister::CashRegister() {
     this->items.clear();
 }
 
+// Empties the items vector
 void CashRegister::clear() {
     this->items.clear();
 }
 
+// Adds an item to the items vector
 void CashRegister::add_item(double item) {
     this->items.push_back(item);
 }
 
+// Adds up the contents of the items vector and returns the total
 double CashRegister::get_total() const {
     double total = 0;
     for (unsigned int i = 0; i < this->items.size(); i++) {
@@ -47,10 +51,12 @@ double CashRegister::get_total() const {
     return total;
 }
 
+// Returns the numbers of items in the vector
 int CashRegister::get_count() const {
     return this->items.size();
 }
 
+// Displays an itemized total of the entire vector with the total
 void CashRegister::display_all() const {
     cout << setfill('*') << setw(12) << "Total";
     cout << setfill('*') << setw(7) << "" << endl;
@@ -60,25 +66,43 @@ void CashRegister::display_all() const {
     }
     cout << "*  Total: " << this->get_total() << "\t *" << endl;
     cout << setfill('*') << setw(19) << "" << endl;
+    cout << endl;
 }
 
 int main() {
-    CashRegister drawer;
+    cout << fixed << setprecision(2);
+    CashRegister cart;
+    char selection;
+    double item;
 
-    
-    drawer.add_item(1.15);
-    drawer.add_item(12.57);
-    drawer.add_item(9.87);
+    cout << "Add item to cart" << endl;
+    cin >> item;
+    cart.add_item(item);
+    cout << "Add more items? press q to quit" << endl;
+    cout << "a -- add item" << endl;
+    cout << "d -- display total" << endl;
+    cout << "e -- clear cart" << endl;
+    cin >> selection;
+    while (selection != 'q') {
+        if (selection == 'a') {
+            cin >> item;
+            cart.add_item(item);
+        } else if (selection == 'd') {
+            cart.display_all();
+        } else if (selection == 'e') {
+            cart.clear();
+        } else {
+            cout << "Make a selection" << endl;
+        }
+        cout << "Add more items? press q to quit" << endl;
+        cout << "a -- add item" << endl;
+        cout << "d -- display total" << endl;
+        cout << "e -- clear cart" << endl;
+        cin >> selection;
+        cout << endl;
+    }
 
-    cout << drawer.get_total() << endl;
-    drawer.display_all();
-
-    drawer.clear();
-    drawer.add_item(6.66);
-
-    cout << drawer.get_total() << endl;
-
-    drawer.display_all();
+    cart.display_all();
 
     return 0;
 }
